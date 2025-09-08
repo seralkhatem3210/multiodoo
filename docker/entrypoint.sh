1b0f349598a4efad0b10bf6e87dfe7668db846ce
uss-alilm <uss_alilm@hotmail.com>
2025-09-08 13:19:28 +0300

    chore: add addons folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
-if [[ "$AUTO_UPGRADE_MODULES" == "1" && -n "$UPGRADE_LIST" ]]; then
-  odoo -c /etc/odoo/odoo.conf -u "$UPGRADE_LIST" --stop-after-init
+
+# auto upgrade modules on start (optional)
+if [[ "${AUTO_UPGRADE_MODULES}" == "1" && -n "${UPGRADE_LIST}" ]]; then
+  echo "[entrypoint] Upgrading modules: ${UPGRADE_LIST}"
+  odoo -c /etc/odoo/odoo.conf -u "${UPGRADE_LIST}" --stop-after-init
 fi
+
 exec "$@"
